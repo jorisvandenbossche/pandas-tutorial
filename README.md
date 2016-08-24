478a7bdf7481d7eded36d19e7d56ee069b08f3ea
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2016-08-24 13:01:17 +0200

    Update for advanced tutorial

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This repository contains the material (notebooks, data) for the pandas tutorial 
 To follow this tutorial you need to have the following packages installed:
 
 - Python version 2.6-2.7 or 3.3-3.5
-- `pandas` version 0.15.2 or later: http://pandas.pydata.org/
+- `pandas` version 0.18.0 or later: http://pandas.pydata.org/ (previous versions will work for most examples as well)
 - `numpy` version 1.7 or later: http://www.numpy.org/
 - `matplotlib` version 1.3 or later: http://matplotlib.org/
 - `ipython` version 3.x with notebook support, or `ipython 4.x` combined with `jupyter`: http://ipython.org
--- convert_notebooks.sh
+++ convert_notebooks.sh
@@ -2,6 +2,6 @@ jupyter nbconvert --to=notebook --config nbconvert_config.py --output "02 - Data
 # jupyter nbconvert --to=notebook --output "03b - Some more advanced indexing.ipynb" "solved - 03b - Some more advanced indexing.ipynb"
 jupyter nbconvert --to=notebook --config nbconvert_config.py --output "03 - Indexing and selecting data.ipynb" "solved - 03 - Indexing and selecting data.ipynb"
 jupyter nbconvert --to=notebook --config nbconvert_config.py --output "04 - Groupby operations.ipynb" "solved - 04 - Groupby operations.ipynb"
-jupyter nbconvert --to=notebook --config nbconvert_config.py --output "04b - Advandec groupby operations.ipynb" "solved - 04b - Advanced groupby operations.ipynb"
+jupyter nbconvert --to=notebook --config nbconvert_config.py --output "04b - Advanced groupby operations.ipynb" "solved - 04b - Advanced groupby operations.ipynb"
 jupyter nbconvert --to=notebook --config nbconvert_config.py --output "05 - Time series data.ipynb" "solved - 05 - Time series data.ipynb"
 jupyter nbconvert --to=notebook --config nbconvert_config.py --output "06 - Reshaping data.ipynb" "solved - 06 - Reshaping data.ipynb"
